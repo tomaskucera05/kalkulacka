b1d40de51f5735a558fe3850c792afa92dd75044
tomaskucera <kuceratomas231@gmail.com>
2025-07-18 18:27:30 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,59 +1,104 @@
-text = input()
-def prevedeni(text):
-    vysledek = []
-    cislo = ""
-    for char in text:
-        if char.isnumeric() or char == ".":
-            cislo += char
-        else:
-            if cislo:
-                vysledek.append(cislo)
-                cislo = ""
-            vysledek.append(char)
-    if cislo:
-        vysledek.append(cislo)
-    return vysledek
-
-def trideni(vysledek):
-    prednost = "*/"
-    i = 1
-    while i < len(vysledek) - 1:
-        prvek = vysledek[i]
-        if prvek in prednost:
-            a = float(vysledek[i+1])
-            b = float(vysledek[i-1])
-            if prvek == "*":
-                vysledek[i-1:i+2] = [str(a*b)]
+expression = input()
+expression = expression.replace(" ", "")
+def tokenize(expression):
+    tokens = []
+    operators = "+-*/"
+    current_number = ""
+    i = 0
+    if i == 0 and expression[i] in "+-":
+        plus_count = 0
+        minus_count = 0
+        while i < len(expression) and expression[i] in "+-":
+            if expression[i] == "+":
+                plus_count += 1
             else:
-                 vysledek[i-1:i+2] = [str(a/b)]
+                minus_count += 1
+            i += 1
+        operator_sequence = "+" if minus_count % 2 == 0 else "-"
+        current_number += operator_sequence
+    while i < len(expression):
+        char = expression[i]
+        if char.isnumeric() or char == ".":
+            current_number += char
+        elif char in operators:
+            if char in "+-":
+                plus_count = 0
+                minus_count = 0
+                while i < len(expression) and expression[i] in "+-":
+                    if expression[i] == "+":
+                        plus_count +=1
+                    else:
+                        minus_count +=1
+                    i += 1
+                operator_sequence = "+" if minus_count % 2 == 0 else "-"
+
+                if tokens and tokens[-1] in "*/":
+                    current_number += operator_sequence
+                else:
+                    if current_number:
+                        tokens.append(current_number)
+                        current_number = ""
+                    tokens.append(operator_sequence)
+                continue    
+            else:
+                if current_number:
+                    tokens.append(current_number)
+                    current_number = ""
+                tokens.append(char)
+        else:
+            raise ValueError
+        i += 1
+    if current_number:
+        tokens.append(current_number)
+    return tokens
+
+def evaluate_multiplication_division(tokens):
+    operators = "*/"
+    i = 1
+    while i < len(tokens) - 1:
+        element = tokens[i]
+        if element in operators:
+            a = float(tokens[i-1])
+            b = float(tokens[i+1])
+            if element == "*":
+                tokens[i-1:i+2] = [str(a*b)]
+            else:
+                 if b == 0:
+                    raise ZeroDivisionError
+                 tokens[i-1:i+2] = [str(a/b)]
             i = 1
         else:
-            i +=2
-        
-    return vysledek        
+            i +=2        
+    return tokens        
 
-def finalni(vysledek):
-    result = float(vysledek[0])
-    znamenka = "+-"
+def evaluate_addition_subtraction(converted_numbers_and_operators):
+    result = float(converted_numbers_and_operators[0])
     i = 1
-    while i < len(vysledek) - 1:
-        prvek = vysledek[i]
-        if prvek in znamenka:
-            if prvek == "+":
-                result += float(vysledek[i+1])
-            else:
-                result -= float(vysledek[i+1])
+    while i < len(converted_numbers_and_operators) - 1:
+        operator = converted_numbers_and_operators[i]
+        if operator == "+":
+            result += float(converted_numbers_and_operators[i+1])
+        elif operator == "-":
+            result -= float(converted_numbers_and_operators[i+1])
         i += 2
     return result
 
-while "(" in text:
-    pozice_prvni_zavorky = text.rfind("(")
-    pozice_druhe_zavorky = text.find(")", pozice_prvni_zavorky)
-    vysledek_zavorky = str(finalni(trideni(prevedeni(text[pozice_prvni_zavorky + 1:pozice_druhe_zavorky]))))
-    text = text[:pozice_prvni_zavorky] + vysledek_zavorky + text[pozice_druhe_zavorky + 1:]
+while "(" in expression:
+    if ")" not in expression:
+        raise ValueError("Unclosed parenthesis")
+    start = expression.rfind("(")
+    end = expression.find(")", start)
+    inner_expression = expression[start + 1:end]
+    inner_tokens = tokenize(inner_expression)
+    simplified_tokens = evaluate_multiplication_division(inner_tokens)
+    inner_result = str(evaluate_addition_subtraction(simplified_tokens))
+    expression = expression[:start] + inner_result + expression[end + 1:]
 try:
-    print(finalni(trideni(prevedeni(text))))
+    final_tokens = tokenize(expression)
+    simplified_tokens = evaluate_multiplication_division(final_tokens)
+    final_result = evaluate_addition_subtraction(simplified_tokens)
+    print(final_result)
 except ValueError:
-    print("Zadej validni input")
-
-            
+    print("Enter a valid input")
+except ZeroDivisionError:
+    print("Division by zero is impossible")
